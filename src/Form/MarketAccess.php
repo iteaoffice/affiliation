0240fd28b610f9a808ad45c6be2c960b9cf3d1eb
Johan van der Heide <info@japaveh.nl>
2020-08-11 08:12:37 +0200

    Added the untracked files

---
+++ src/Form/MarketAccess.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * ITEA Office all rights reserved
+ *
+ * @category    Project
+ *
+ * @author      Johan van der Heide <johan.van.der.heide@itea3.org>
+ * @copyright   Copyright (c) 2019 ITEA Office (https://itea3.org)
+ */
+
+declare(strict_types=1);
+
+namespace Affiliation\Form;
+
+use Laminas\Form\Element\Csrf;
+use Laminas\Form\Element\Submit;
+use Laminas\Form\Element\Text;
+use Laminas\Form\Element\Textarea;
+use Laminas\Form\Form;
+use Laminas\InputFilter\InputFilterProviderInterface;
+
+/**
+ * Class MarketAccess
+ * @package Affiliation\Form
+ */
+final class MarketAccess extends Form implements InputFilterProviderInterface
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setAttribute('method', 'post');
+        $this->setAttribute('action', '');
+        $this->setAttribute('class', 'form-horizontal');
+
+        $this->add(
+            [
+                'type'       => Textarea::class,
+                'name'       => 'marketAccess',
+                'attributes' => [
+                    'rows'        => 10,
+                    'label'       => _('txt-affiliation-market-access-label'),
+                    'placeholder' => _('txt-affiliation-market-access-placeholder'),
+                ],
+                'options'    => [
+                    'help-block' => _('txt-affiliation-market-access-help-block'),
+                ],
+            ]
+        );
+
+        $this->add(
+            [
+                'type' => Csrf::class,
+                'name' => 'csrf',
+            ]
+        );
+        $this->add(
+            [
+                'type'       => Submit::class,
+                'name'       => 'submit',
+                'attributes' => [
+                    'class' => 'btn btn-primary',
+                    'value' => _('txt-update'),
+                ],
+            ]
+        );
+        $this->add(
+            [
+                'type'       => Submit::class,
+                'name'       => 'cancel',
+                'attributes' => [
+                    'class' => 'btn btn-warning',
+                    'value' => _('txt-cancel'),
+                ],
+            ]
+        );
+    }
+
+    public function getInputFilterSpecification(): array
+    {
+        return [
+            'marketAccess' => [
+                'required' => true,
+                'filters'  => [
+                    [
+                        'name' => 'StringTrim',
+                    ],
+                    [
+                        'name' => 'StripTags',
+                    ],
+                ],
+            ],
+        ];
+    }
+}
